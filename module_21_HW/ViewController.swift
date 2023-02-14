dc1093df13df670b04186c58c6a77d657d87b348
Egor Kruglov <yegorkruglov@gmail.com>
2023-02-14 22:12:32 +0300

    Initial Commit

---
+++ module_21_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  module_21_HW
+//
+//  Created by Egor Kruglov on 14.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
