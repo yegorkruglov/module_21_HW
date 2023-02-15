d0626625b54c014d563d063a10ab9d6018a0c7e5
Egor Kruglov <yegorkruglov@gmail.com>
2023-02-15 13:20:48 +0300

    added pan gestures to circles

--- module_21_HW/ViewController.swift
+++ module_21_HW/ViewController.swift
@@ -29,6 +29,77 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    
+    @IBAction func panCircle1(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 1 was panned")
+    }
+    
+    @IBAction func panCircle2(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 2 was panned")
+    }
+    
+    @IBAction func panCircle3(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 3 was panned")
+    }
+    
+    @IBAction func panCircle4(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 4 was panned")
+    }
+    
+    @IBAction func panCircle5(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 5 was panned")
+    }
+    
+    @IBAction func panCircle6(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 6 was panned")
+    }
+    
+    @IBAction func panCircle7(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 7 was panned")
+    }
+    
+    @IBAction func panCircle8(_ gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        print("Circle 8 was panned")
+    }
+    
 }
-
