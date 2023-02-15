159bcd91c94beede3127c1c320fbe82df80e455b
Egor Kruglov <yegorkruglov@gmail.com>
2023-02-15 12:42:13 +0300

    start

---
+++ module_21_HW/MyCustomView.swift
@@ -0,0 +1,75 @@
+//
+//  MyCustomView.swift
+//  module_21_HW
+//
+//  Created by Egor Kruglov on 14.02.2023.
+//
+
+import UIKit
+
+@IBDesignable class MyCustomView: UIView {
+    
+    
+    @IBOutlet weak var labelForView: UILabel!
+    
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBInspectable var circleColor: UIColor {
+        get {
+            return colorView.backgroundColor!
+        }
+        set(circleColor) {
+            colorView.backgroundColor = circleColor
+        }
+    }
+    
+    @IBInspectable var labelText: String {
+        get {
+            return labelForView.text!
+        }
+        set(labelText) {
+            labelForView.text = labelText
+        }
+    }
+    
+//    @IBInspectable var labelBackgroundColor: UIColor {
+//        get {
+//            return labelForView.backgroundColor!
+//        }
+//        set(labelColor) {
+//            labelForView.backgroundColor = labelColor
+//        }
+//    }
+    
+    var workingView: UIView!
+    
+    var xibName: String = "MyCustomView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    func getFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+        return view
+    }
+    
+    func setCustomView() {
+        workingView = getFromXib()
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        workingView.layer.cornerRadius = frame.size.width / 2
+        addSubview(workingView)
+    }
+    
+}
+
--- module_21_HW/ViewController.swift
+++ module_21_HW/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circle1: MyCustomView!
+    
+    @IBOutlet weak var circle2: MyCustomView!
+    
+    @IBOutlet weak var circle3: MyCustomView!
+    
+    @IBOutlet weak var circle4: MyCustomView!
+    
+    @IBOutlet weak var circle5: MyCustomView!
+    
+    @IBOutlet weak var circle6: MyCustomView!
+        
+    @IBOutlet weak var circle7: MyCustomView!
+    
+    @IBOutlet weak var circle8: MyCustomView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
